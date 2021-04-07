7853aa8f1e8375a51d348d93b87828aaaa8f58ad
TedBaggins <tedbagginsrb@gmail.com>
2021-04-07 13:59:52 +0300

    fix cors

--- app.js
+++ app.js
@@ -6,10 +6,21 @@ const cors = require("cors");
 const app = express();
 
 const port = 3080;
-var corsOptions = {
-    origin: "http://localhost:3080"
-};
-app.use(cors(corsOptions));
+let whitelist = ['http://localhost:3080', 'http://localhost:8080']
+// let corsOptions = {
+//     origin: "http://localhost:3080"
+// };
+// let corsOptions = {
+//     origin: function (origin, callback) {
+//         if (whitelist.indexOf(origin) !== -1) {
+//             callback(null, true)
+//         } else {
+//             callback(new Error('Not allowed by CORS'))
+//         }
+//     }
+// }
+// app.use(cors(corsOptions));
+app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.listen(port, () => {
